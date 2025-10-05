b1450fa001b976b3bee851b813e76beeafda28fa
DevJPaiva <jorgefe0210@gmail.com>
2025-10-05 15:38:31 -0300

    Add simple number guessing game
    Introduces a basic number guessing game using alert and prompt. The user is asked to guess a secret number between 1 and 30, with feedback provided via console messages.

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,11 @@
+alert('Boas vindas!');
+
+let secretNumber = 29;
+
+let kick = prompt('Choose a number between 1 and 30');
+
+if (secretNumber == kick) {
+    console.log('WOW!! You win');
+} else {
+    console.log('oh no, try again!')
+}
