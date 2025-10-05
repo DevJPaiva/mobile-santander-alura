2dd2b5080749f9b4d52767b50081a3044cbab270
DevJPaiva <jorgefe0210@gmail.com>
2025-10-05 15:43:36 -0300

    Translate app messages to English
    Changed alert, prompt, and console messages in app.js from Portuguese to English for better accessibility and consistency.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,11 @@
-alert('Boas vindas!');
+alert("Welcome!");
 
 let secretNumber = 29;
 
-let kick = prompt('Choose a number between 1 and 30');
+let kick = prompt("Choose a number between 1 and 30");
 
 if (secretNumber == kick) {
-    console.log('WOW!! You win');
+  console.log("WOW!! You win");
 } else {
-    console.log('oh no, try again!')
+  console.log("oh no, try again!");
 }
