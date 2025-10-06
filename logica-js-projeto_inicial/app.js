1401512764afcd96b06bb82b918ff8cd57d309fa
DevJPaiva <jorgefe0210@gmail.com>
2025-10-06 10:45:41 -0300

    Track number of user attempts in guessing game
    Added a kicksUser variable to count the number of guesses the user makes before finding the secret number. The win message now displays the total attempts. Removed unnecessary break statement to allow message display.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,19 +1,21 @@
 alert("Welcome!");
 
 let secretNumber = 4;
-let kick
+let kick;
+let kicksUser =1;
 
 while (secretNumber != kick) {
   kick = prompt("Choose a number between 1 and 10");
 
   if (secretNumber == kick) {
-    console.log("WOW!! You win");
-    break;
+    console.log("WOW!! You win with " + kicksUser + " kicks.");
+    //break;
   } else {
     if (kick > secretNumber) {
       alert("The secret number is lower than " + kick);
     } else {
       alert("The secret number is higher than " + kick);
     }
+    kicksUser++;
   }
 }
