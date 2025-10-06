c2ef70970d348696300b6ceaa5ce5fe05ade043f
DevJPaiva <jorgefe0210@gmail.com>
2025-10-06 09:55:36 -0300

    Add loop and hints to number guessing game
    Replaces single guess logic with a loop that continues until the correct number is guessed. Adds hints to inform the user if the secret number is higher or lower than their guess. Updates the secret number range to 1-10.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,17 @@
 alert("Welcome!");
 
-let secretNumber = 29;
+let secretNumber = 4;
 
-let kick = prompt("Choose a number between 1 and 30");
+while (secretNumber != kick) {
+  let kick = prompt("Choose a number between 1 and 10");
 
-if (secretNumber == kick) {
-  console.log("WOW!! You win");
-} else {
-  console.log("oh no, try again!");
+  if (secretNumber == kick) {
+    console.log("WOW!! You win");
+  } else {
+    if (kick > secretNumber) {
+      alert("The secret number is lower than " + kick);
+    } else {
+      alert("The secret number is higher than " + kick);
+    }
+  }
 }
