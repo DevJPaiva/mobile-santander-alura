9595b305b94328b4d3225b0edfda12315370aa7b
DevJPaiva <jorgefe0210@gmail.com>
2025-10-06 17:50:40 -0300

    Add test for number sign detection in ex08.js
    Introduces a test script that prompts the user for a number and prints whether it is positive, negative, or zero using if-else statements.

---
+++ logica-js-projeto_inicial/test/ex08.js
@@ -0,0 +1,11 @@
+//Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
+
+let numero = prompt('Digite um numero');
+
+if (numero > 0){
+  console.log('O numero é positivo');  
+} else if (numero < 0){
+  console.log('O numero é negativo')
+} else{
+  console.log('o numero é zero')
+}
