1c9bbc6a3a6a9c4ad05e086ba223fded7d56fdc0
DevJPaiva <jorgefe0210@gmail.com>
2025-10-15 10:30:09 -0300

    Refactor text display logic in app.js
    Commented out direct DOM manipulation and introduced exibirTextNaTela function to update text content for specified tags. This improves code reusability and separation of concerns.

--- js-curso-2-desafio_1/app.js
+++ js-curso-2-desafio_1/app.js
@@ -1,8 +1,15 @@
+/*
 let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Hora do desafio!';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+*/
+
+function exibirTextNaTela(tag,texto) {
+    let campo =  document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
 function verificarChute() {
     console.log('o botão foi clicado')
