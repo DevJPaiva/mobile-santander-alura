9f3acac3b0fd5fd7e3c8c5922d1868edb40bd5b8
DevJPaiva <jorgefe0210@gmail.com>
2025-10-16 20:00:03 -0300

    Add IMC calculation function
    Introduces a function to calculate the body mass index (IMC) given height and weight, and displays the result using alert. Includes an example call with sample values.

---
+++ js-curso-2-desafio_1/functions2.js
@@ -0,0 +1,7 @@
+/*Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.*/
+
+function calculoDeIMC(altura, peso) {
+    alert(imc = peso / (altura*altura));
+}
+
+calculoDeIMC(1.72, 70);
