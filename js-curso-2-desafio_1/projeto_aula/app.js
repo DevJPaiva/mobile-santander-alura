14da09e99750bd92113b86ebf40017002b9e6a60
DevJPaiva <jorgefe0210@gmail.com>
2025-10-16 11:46:39 -0300

    Adiciona contagem de tentativas ao jogo do número secreto
    Agora o jogo informa ao usuário quantas tentativas foram necessárias para acertar o número secreto, exibindo a mensagem adequada no acerto. Incremento da variável de tentativas a cada tentativa incorreta.

--- js-curso-2-desafio_1/projeto_aula/app.js
+++ js-curso-2-desafio_1/projeto_aula/app.js
@@ -1,39 +1,32 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela("h1", "Jogo do número secreto");
+exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 
 function verificarChute() {
-    let chute = document.querySelector('input').value;
+  let chute = document.querySelector("input").value;
 
-    if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1','Parabéns!');
-        exibirTextoNaTela('p', 'Você descobriu o número secreto.');
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela("h1", "Parabéns!");
+    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+    exibirTextoNaTela("p", mensagemTentativas);
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela("p", "O número secreto é menor.");
     } else {
-        if (chute > numeroSecreto) {
-            exibirTextoNaTela('p', 'O número secreto é menor.');
-        } else {
-            exibirTextoNaTela('p', 'O número secreto é maior.');
-        }
+      exibirTextoNaTela("p", "O número secreto é maior.");
     }
+    tentativas++;
+  }
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+  return parseInt(Math.random() * 10 + 1);
 }
-
-
-
-
-
-
-
-
-
-
-
